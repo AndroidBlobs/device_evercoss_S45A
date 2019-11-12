fe30b48d369b358fbd5d4ec71d344cbe619ad6ed
ShivamKumarJha <jha.shivam3@gmail.com>
2019-11-12 21:48:10 +0000

    sp9832e_1h10_gofu_osll-user 8.1.0 OPM2.171019.012 35310 release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,8 @@
+DEVICE_PATH := device/evercoss/S45A
+BOARD_VENDOR := evercoss
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+
+-include vendor/evercoss/S45A/BoardConfigVendor.mk
